9e8f15c0a826d532b5438aaa124cbaeddad1fb58
OchAbdo <ochabdo02@gmail.com>
2026-01-22 14:39:43 +0100

    Add typeOrm config pg validator and transformer. make the connection with database and add the pipeGlobal in main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }),
+  TypeOrmModule.forRootAsync({
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: (configService: ConfigService) => ({
+      type: 'postgres',
+      host: configService.get<string>('HOSTDB'),
+      port: configService.get<number>('PORTDB'),
+      username: configService.get<string>('USERNAMEDB'),
+      password: configService.get<string>('PASSWORDDB'),
+      database: configService.get<string>('DATABASENAME'),
+      autoLoadEntities: true,
+      synchronize: true,
+
+    })
+  })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule { }
